3612ac57fa3a3f1b2dc50a0845bc0071e5d5f419
Daniel Oliveira <oliveira.danielm@gmail.com>
2022-01-12 00:35:28 -0300

    First commit

---
+++ test/models/post_test.rb
@@ -0,0 +1,12 @@
+require "test_helper"
+
+class PostTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+
+  test "should not save post without title" do
+    post = Post.new
+    assert_not post.save, "Saved the post without a title"
+  end
+end
